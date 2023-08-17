8915e3188329335cc31d107dbbcd0c6d8950b146
Pedro Gonçalves de Oliveira <105668982+Pedry123@users.noreply.github.com>
2023-08-17 11:28:58 -0400

    Add files via upload

---
+++ redesocial/RedeSocial/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Video ohogas = new Video(1000000, 720, 480, "mp4", 10, 1080);
+        ohogas.curtir();
+    }
+}
---
+++ redesocial/RedeSocial/src/Post.java
@@ -0,0 +1,19 @@
+public class Post {
+
+
+    public void comentar(String comentario) {
+        System.out.println(comentario);
+    }
+
+
+    public void curtir() {
+        System.out.println("Post curtido.");
+    }
+
+
+    public void compartilhar() {
+        System.out.println("Post compartilhado.");
+    }
+
+
+}
