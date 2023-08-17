84eb26997df6fc916412a5b7b4694c20aabb4841
Pedro Gonçalves de Oliveira <105668982+Pedry123@users.noreply.github.com>
2023-08-17 11:16:57 -0400

    Add files via upload

---
+++ redesocial/RedeSocial/src/Video.java
@@ -0,0 +1,27 @@
+public class Video {
+    private int duracao;
+    private int resolucao;
+
+    public Video(int duracao, int resolucao) {
+        this.duracao = duracao;
+        this.resolucao = resolucao;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public int getResolucao() {
+        return resolucao;
+    }
+
+    public void setResolucao(int resolucao) {
+        this.resolucao = resolucao;
+    }
+
+
+}
